cd5a392b6852445b84dcda6af5e2e2ee097c3fca
ronashkenazy <ronashkenazy04@gmail.com>
2023-09-13 15:21:43 -0400

    pushing both files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] placeHolder = new String[0];
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.main(placeHolder);
+
+    }
+}
